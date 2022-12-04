8cc2d16520a94363ecd25e26bddbaa3275517790
Ingmar Steiner <ingmar.steiner@iais.fraunhofer.de>
2022-12-04 07:50:43 +0100

    Add version to pandoc extension, test on Ubuntu Focal

--- src/main/groovy/org/m2ci/msp/pandocreveal/PandocExtension.groovy
+++ src/main/groovy/org/m2ci/msp/pandocreveal/PandocExtension.groovy
@@ -1,8 +1,11 @@
 package org.m2ci.msp.pandocreveal
 
 import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
 
 interface PandocExtension {
+
+    Property<String> getVersion()
 
     RegularFileProperty getBinary()
 
