913dfb8dd247bce285c9be88558b1011a36409a0
Ingmar Steiner <steiner@coli.uni-saarland.de>
2024-02-26 15:48:57 +0100

    Upgrade reveal.js to v5.0.5

--- src/main/groovy/org/m2ci/msp/pandocreveal/PandocRevealPlugin.groovy
+++ src/main/groovy/org/m2ci/msp/pandocreveal/PandocRevealPlugin.groovy
@@ -62,7 +62,7 @@ class PandocRevealPlugin implements Plugin<Project> {
 
         project.configurations.maybeCreate REVEALJS
 
-        project.ext.revealJsVersion = '5.0.4'
+        project.ext.revealJsVersion = '5.0.5'
 
         switch (OperatingSystem.current()) {
             case { it.isLinux() }:
