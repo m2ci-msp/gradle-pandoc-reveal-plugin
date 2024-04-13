033466ed0a8803ee6e3a220b92fa079cddb470a9
Ingmar Steiner <ingmar.steiner@iais.fraunhofer.de>
2024-04-13 12:02:14 +0200

    Upgrade Pandoc to v3.1.13

--- src/main/groovy/org/m2ci/msp/pandocreveal/PandocRevealPlugin.groovy
+++ src/main/groovy/org/m2ci/msp/pandocreveal/PandocRevealPlugin.groovy
@@ -57,7 +57,7 @@ class PandocRevealPlugin implements Plugin<Project> {
 
         project.tasks.register('pandoc', UnpackPandoc) {
             config.set pandocConfig.name
-            version.set '3.1.12.2'
+            version.set '3.1.13'
         }
 
         project.configurations.maybeCreate REVEALJS
