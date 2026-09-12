8f8cee6e9e6c570dc409e9c730f6f2ecc3307752
Ingmar Steiner <ingmar.steiner@iais.fraunhofer.de>
2026-09-12 17:05:13 +0200

    Upgrade reveal.js to v6.0.2

--- src/main/groovy/org/m2ci/msp/pandocreveal/PandocRevealExtension.groovy
+++ src/main/groovy/org/m2ci/msp/pandocreveal/PandocRevealExtension.groovy
@@ -15,7 +15,7 @@ class PandocRevealExtension {
     final Property<String> pandocVersion = project.objects.property(String)
             .convention('3.11')
     final Property<String> revealVersion = project.objects.property(String)
-            .convention('6.0.1')
+            .convention('6.0.2')
 
     final RegularFileProperty markdownFile = project.objects.fileProperty()
     final RegularFileProperty headerFile = project.objects.fileProperty()
